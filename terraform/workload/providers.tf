ffeb735266dbb413470f23fe03561fde4b2da4c6
Kevin Zheng <k.zheng7077@gmail.com>
2026-01-11 17:05:28 -0500

    Add default environment and ownership tags to AWS provider

--- terraform/workload/providers.tf
+++ terraform/workload/providers.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+        Project = "aws-secure-multi-account-foundation"
+        Environment = "lab"
+        Owner = "KevinZheng"
+    }
+  }
 }
